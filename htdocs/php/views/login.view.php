8ed10b2a16518cce56a1aae5c4c648f1a64512f8
Milito97 <90428595+Milito97@users.noreply.github.com>
2023-03-11 06:07:36 +0000

    added header and footer

--- htdocs/php/views/login.view.php
+++ htdocs/php/views/login.view.php
@@ -1,27 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../css/stylesheet.css">
-    <title>Login Page</title>
-</head>
-
-<body>
-    <div class="login-container">
-        <h2>Login</h2>
-        <form action="login.php" method="POST">
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" placeholder="User Name" required>
-            <br>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" placeholder="Password" required>
-            <br>
-            <button type="submit" name="login">Login</button>
-        </form>
-    </div>
-</body>
-
-</html>
+<?php require 'partials/head.php'; ?>
+<div class="login-container">
+    <h2>Login</h2>
+    <form action="login.php" method="POST">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" placeholder="User Name" required>
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" placeholder="Password" required>
+        <br>
+        <button type="submit" name="login">Login</button>
+    </form>
+</div>
+<?php require 'partials/foot.php'; ?>
